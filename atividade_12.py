c764f3d2e6acde7a4e0f7a9ae40703fc05767dc3
GustavoGaleno376 <gustabrffmano@gmail.com>
2024-09-11 18:30:39 -0300

    media da nota do aluno

---
+++ atividade_12.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+nota_media=float(input("digite sua média"))
+if (nota_media>=7):
+    print("você está aprovado")
+else:
+    print("você não está aprovado")
